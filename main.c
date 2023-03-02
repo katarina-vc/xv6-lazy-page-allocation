d9ef40666271424fde72a162c9c3477903d0586f
katarina-vc <kvc750@gmail.com>
2023-03-01 23:50:39 -0500

    removed dumb code

--- main.c
+++ main.c
@@ -35,8 +35,6 @@ main(void)
   kinit2(P2V(4*1024*1024), P2V(PHYSTOP)); // must come after startothers()
   userinit();      // first user process
   mpmain();        // finish this processor's setup
-     cprintf("xv6 kernel is booting\n");
-
 
 }
 
